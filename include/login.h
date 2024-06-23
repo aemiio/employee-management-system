6799b47e6f44636c906759b188c585ec2bdd689b
aem <111235272+aemiio@users.noreply.github.com>
2024-06-23 13:26:00 +0800

    fix: login.h and main.cpp

--- include/login.h
+++ include/login.h
@@ -9,28 +9,23 @@
 using namespace std;
 
 void loginMenu();
+
 void adminLogin();
+
 void userLogin();
 
-int accNum = 5;
-int idNum[20] =
-        {1, 2, 3, 4, 5};
-char empType[50][20] =
-        {"Admin Officer", "Utility", "Utility", "Secretary", "Admin Aide"};
-char username[50][20] =
-        {"jose", "wally", "paolo", "Vic", "joey"};
-char password[50][20] =
-        {"1111", "2222", "3333", "4444", "5555"};
-int balance[] =
-        {0, 0, 0, 0, 0};
-char fName[50][20] =
-        {"Jose", "wally", "paolo", "Vic", "Joey"};
-char lName[50][20] =
-        {"manalo", "bayola", "balesteros", "sotto", "deleon"};
-int banned[50] = {0};
-int choice = 0;
-int logged= 0;
-int tries = 0;
-int i = 0;
+extern int accNum;
+extern int idNum[20];
+extern char empType[50][20];
+extern char username[50][20];
+extern char password[50][20];
+extern int balance[];
+extern char fName[50][20];
+extern char lName[50][20];
+extern int banned[50];
+extern int choice;
+extern int logged;
+extern int tries;
+extern int i;
 
 #endif
